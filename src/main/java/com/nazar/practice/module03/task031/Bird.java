5737cddad03a27586d87aba0c28eda7e89014b66
YanovetSS <yanovetswork@gmail.com>
2017-02-16 11:37:53 +0200

    Module3

--- src/main/java/com/nazar/practice/module03/task031/Bird.java
+++ src/main/java/com/nazar/practice/module03/task031/Bird.java
@@ -4,12 +4,37 @@ package com.nazar.practice.module03.task031;
  * Created by MakeMeSm1Le- on 14.02.2017.
  */
 public class Bird {
-    public static void main(String[] args) {
-        Bird someBird = new Bird();
-        someBird.singBird();
+    private final String name;
+
+    private final String variable;
+
+    public Bird(String name) {
+        this.name = name;
+        variable = "\nI am " + name +
+                "\nI am walking  " +
+                "\nI am flying  " +
+                "\nI am singing " +
+                "\nI am Bird";
     }
-    
-    public void singBird() {
-        System.out.println("I am walking" + "\nI am flying" + "\nI am singing" + "\nI am Bird");
+
+    public Bird() {
+        name = null;
+        variable =
+                "\nI am walking  " +
+                        "\nI am flying  " +
+                        "\nI am singing " +
+                        "\nI am Bird";
+    }
+
+    public static void main(String[] args) {
+        Bird birdShpak = new Bird("Shpak");
+        Bird bird = new Bird();
+
+        bird.sing();
+        birdShpak.sing();
+    }
+
+    public void sing() {
+        System.out.println(variable);
     }
 }
