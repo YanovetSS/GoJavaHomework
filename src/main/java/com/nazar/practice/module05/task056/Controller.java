2062ce8fc57aa16b6a0ea5ed1dde6bca4cb0affc
YanovetSS <yanovetswork@gmail.com>
2017-03-28 21:35:41 +0300

    Module5

--- src/main/java/com/nazar/practice/module05/task056/Controller.java
+++ src/main/java/com/nazar/practice/module05/task056/Controller.java
@@ -7,20 +7,39 @@ import com.nazar.practice.module05.task053.GoogleAPI;
 import com.nazar.practice.module05.task053.TripAdvisorAPI;
 import com.nazar.practice.module05.task055.DAO;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Controller {
     private DAO dao = new DAO();
-    private API firstAPI = new BookingComAPI();
-    private API secondAPI = new TripAdvisorAPI();
-    private API thirdAPI = new GoogleAPI();
 
-    private API[] apies = {firstAPI, secondAPI, thirdAPI};
+
+    public Controller() {
+         API firstAPI = new BookingComAPI();
+         API secondAPI = new TripAdvisorAPI();
+         API thirdAPI = new GoogleAPI();
+         apies = new API[]{firstAPI, secondAPI, thirdAPI};
+    }
+
+    private API[] apies ;
 
     public Room[] requestRooms(int price, int persons, String hotel, String city) {
-        System.out.println("Result: ");
         for (API api : apies) {
             dao.addRooms(api.findRooms(price, persons, hotel, city));
         }
         return dao.getRoomDB();
     }
-
+        public  Room[] check(API api1, API api2){
+            List<Room> listOfEqualRooms = new ArrayList();
+            for (Room roomFirstAPI : api1.getRooms()) {
+                for (Room roomSecondAPI : api2.getRooms()) {
+                    if (roomFirstAPI.equals(roomSecondAPI)) {
+                        listOfEqualRooms.add(roomFirstAPI);
+                        listOfEqualRooms.add(roomSecondAPI);
+                    }
+                }
+            }
+            System.out.println(listOfEqualRooms.size() + " equals rooms found!");
+            return listOfEqualRooms.toArray(new Room[listOfEqualRooms.size()]);
+        }
 }
