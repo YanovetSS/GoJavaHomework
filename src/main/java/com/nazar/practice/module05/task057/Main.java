91261b0906935c2194d1a7b531de823230a50035
YanovetSS <yanovetswork@gmail.com>
2017-03-28 21:36:34 +0300

    Module5

--- src/main/java/com/nazar/practice/module05/task057/Main.java
+++ src/main/java/com/nazar/practice/module05/task057/Main.java
@@ -1,14 +1,22 @@
 package com.nazar.practice.module05.task057;
 
+import com.nazar.practice.module05.task053.API;
+import com.nazar.practice.module05.task053.BookingComAPI;
+import com.nazar.practice.module05.task053.GoogleAPI;
+import com.nazar.practice.module05.task053.TripAdvisorAPI;
 import com.nazar.practice.module05.task056.Controller;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        API firstAPI = new BookingComAPI();
+        API secondAPI = new TripAdvisorAPI();
+        API thirdAPI = new GoogleAPI();
         Controller objectForDoSomethink = new Controller();
         objectForDoSomethink.requestRooms(100, 1, "Ukraine", "Kiev");
         objectForDoSomethink.requestRooms(400, 4, "Sonce", "Lviv");
         objectForDoSomethink.requestRooms(1,1,"lalal","lalal");
+        System.out.println();
+        System.out.println(objectForDoSomethink.check(firstAPI, thirdAPI));
     }
 }
