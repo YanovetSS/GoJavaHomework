a45a98e1dcac87f580da5c225ef97cae5d756354
YanovetSS <yanovetswork@gmail.com>
2017-05-02 19:26:46 +0300

    Module11

---
+++ src/main/java/com/nazar/practice/module11/Main.java
@@ -0,0 +1,31 @@
+package com.nazar.practice.module11;
+
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<String, String> wordsToReplace = new HashMap<>();
+        wordsToReplace.put("hehe", "Bigg BoSS");
+        wordsToReplace.put("arara", "BATYA");
+        try {
+            TextFileOperations fileWithSampleText = TextFileOperations.getInstance();
+            System.out.println("Initial file contents:\n" + fileWithSampleText.readFileContentsToString());
+            System.out.println("\nString with replaced words:");
+            System.out.println(fileWithSampleText.replacer(wordsToReplace));
+            System.out.print("\nRewriting file with the new string... ");
+            fileWithSampleText.fileContentsReplacer(wordsToReplace);
+            System.out.println("Done!");
+            System.out.print("Appending new string to file... ");
+            fileWithSampleText.fileContentsMerge(wordsToReplace);
+            System.out.println("Done!");
+            System.out.println("\nResulting file contents:\n" + fileWithSampleText.readFileContentsToString());
+            System.out.println("\nNumber of occurrences of word \"Nazar\": " + fileWithSampleText.checkWord("Nazar"));
+            System.out.println("Number of occurrences of word \"Yanovets\": " + fileWithSampleText.checkWord("Yanovets"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/nazar/practice/module11/TextFileOperations.java
@@ -0,0 +1,111 @@
+package com.nazar.practice.module11;
+
+import java.io.*;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class TextFileOperations {
+    private static TextFileOperations fileWithSampleText;
+    private File textFile;
+
+    private TextFileOperations() throws IOException {
+        makeEmptyTextFile("Yanovets.txt");
+        fillFileWithSampleText();
+    }
+
+    public static synchronized TextFileOperations getInstance() throws IOException {
+        if (fileWithSampleText == null) {
+            fileWithSampleText = new TextFileOperations();
+        }
+        return fileWithSampleText;
+    }
+
+    private void makeEmptyTextFile(String fileName) throws IOException {
+        textFile = new File(fileName);
+        try {
+            if (!textFile.createNewFile()) {
+                new PrintWriter(fileName).close();
+            }
+        } catch (IOException e) {
+            System.out.println("Error creating file!");
+            throw e;
+        }
+    }
+
+    private void fillFileWithSampleText() throws IOException {
+        try (PrintWriter fileWriter = new PrintWriter(new FileWriter(textFile))) {
+            fileWriter.println("Nazar Yanovets hehe");
+            fileWriter.println("Oleg Yanovets arara");
+        } catch (IOException e) {
+            System.out.println("Error initialising file!");
+            throw e;
+        }
+    }
+
+    public File fileContentsReplacer(Map<String, String> wordsToReplace) throws IOException {
+        String outputString = replacer(wordsToReplace);
+        return writeContentsToFile(outputString, false);
+
+    }
+
+    public File fileContentsMerge(Map<String, String> wordsToReplace) throws IOException {
+        String outputString = replacer(wordsToReplace);
+        return writeContentsToFile(outputString, true);
+    }
+
+    public String readFileContentsToString() throws IOException {
+        StringBuilder outputString = new StringBuilder();
+        try (BufferedReader bufferedFileReader = new BufferedReader(new FileReader(textFile))) {
+            outputString.append(bufferedFileReader.lines().collect(Collectors.joining(System.lineSeparator())));
+        } catch (IOException e) {
+            System.out.println("Error reading file!");
+            throw e;
+        }
+        return outputString.toString();
+    }
+
+    public String replacer(Map<String, String> wordsToReplace) throws IOException {
+        String outputString = readFileContentsToString();
+        for (Map.Entry<String, String> entry : wordsToReplace.entrySet()) {
+            outputString = outputString.replaceAll(entry.getKey(), entry.getValue());
+        }
+        return outputString;
+    }
+
+    public File writeContentsToFile(String contents, boolean append) throws IOException {
+        try (BufferedWriter bufferedFileWriter = new BufferedWriter(new FileWriter(textFile, append))) {
+            bufferedFileWriter.write(contents);
+            bufferedFileWriter.newLine();
+        } catch (IOException e) {
+            System.out.println("Error writing to file");
+            throw e;
+        }
+        return textFile;
+    }
+
+    public int checkWord(String word) throws IOException {
+        return readFileContentsToString().split(word).length - 1;
+    }
+
+
+    public String readFileContentsToStringJava6() throws IOException {
+        StringBuilder outputString = new StringBuilder();
+        String textLine;
+        BufferedReader bufferedFileReader = null;
+        try {
+            bufferedFileReader = new BufferedReader(new FileReader(textFile));
+            while ((textLine = bufferedFileReader.readLine()) != null) {
+                outputString.append(textLine);
+                outputString.append(System.lineSeparator());
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading file!");
+            throw e;
+        } finally {
+            if (bufferedFileReader != null) {
+                bufferedFileReader.close();
+            }
+        }
+        return outputString.toString();
+    }
+}
